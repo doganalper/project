2e78204e902d2aceda972ea4f0defc4f72163a80
Alper Dogan <alperuni217@gmail.com>
2021-04-10 17:31:32 +0300

    vue-unicons added to project

--- client/src/main.js
+++ client/src/main.js
@@ -2,8 +2,10 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
+import Unicon from './utils/Icons';
 
 Vue.config.productionTip = false;
+Vue.use(Unicon);
 
 new Vue({
     router,
---
+++ client/src/utils/Icons.js
@@ -0,0 +1,10 @@
+import Unicon from 'vue-unicons/dist/vue-unicons-vue2.umd';
+import {
+    uniSignout,
+} from 'vue-unicons/dist/icons';
+
+Unicon.add([
+    uniSignout
+])
+
+export default Unicon;
